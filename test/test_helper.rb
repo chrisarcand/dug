dd07d137598fdf2ed0ad3136ab7706d14fa9601d
Chris Arcand <chris@chrisarcand.com>
2016-02-03 06:50:25 -0500

    Use simplecov with codeclimate as just another formatter
    Simplecov is good; I had it added before but tossed it when I wanted to
    add codeclimate's gem. TIL you can just use codeclimate as another
    simplecov formatter, so this adds it back to get the nice HTML output
    from simplecov once again.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+require 'codeclimate-test-reporter'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+])
+SimpleCov.start
 
 require 'dug'
 require 'minitest/autorun'
