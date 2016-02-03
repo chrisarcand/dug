9d5bee56e917a0df6621fe1ff418f453ce671f20
Chris Arcand <chris@chrisarcand.com>
2016-02-03 07:53:39 -0500

    Nevermind, Code Climate wrapper sucks.
    Undos #1 because the CC wrapper to SimpleCov sucks and doesn't defer to
    SimpleCov when run locally without posting to CC. Very obtrusive. Just
    run SimpleCov normally with excluded files to match CC's run.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'simplecov'
-require 'codeclimate-test-reporter'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
-])
-SimpleCov.start
+# Because Code Climate stupidly doesn't defer to SimpleCov and
+# run if it's not actually posting to CC (not by design), so you
+# can't just add another formatter to SimpleCov as they claim.
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/test/'
+    add_filter '/lib/dug/version.rb'
+  end
+end
 
 require 'dug'
 require 'minitest/autorun'
