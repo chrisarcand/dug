8e2a517d1465ecf6cf085c1eb5245c4fc512498c
Drew Bomhof <dbomhof@redhat.com>
2016-11-17 15:41:31 -0500

    Fixed the cert error by declaring the cert_path
    Set the ENV['SSL_CERT_FILE'] using the requisite cert_path

--- templates/script.rb
+++ templates/script.rb
@@ -1,10 +1,12 @@
 require 'dug'
+cert_path = Gem.loaded_specs['google-api-client'].full_gem_path+'/lib/cacerts.pem'
+ENV['SSL_CERT_FILE'] = cert_path
 
 Dug.configure do |config|
  # You can alternatively pass environment variables
  # or a path to a downloadable authentication .json file from Google
- config.client_id = "<google oauth client id>"
- config.client_secret = "<google secret>"
+ config.client_id = "<google client id>"
+ config.client_secret = "<google client secret>"
 
  config.rule_file = File.join(Dir.pwd, "/dug/dug_rules.yml")
 end
